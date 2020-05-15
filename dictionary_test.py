b657e8ef9930e4aa2da008d25d110b517bcc1ce3
GrahamOHagan <64635731+GrahamOHagan@users.noreply.github.com>
2020-05-15 14:58:03 +0100

    Update dictionary_test.py

--- dictionary_test.py
+++ dictionary_test.py
@@ -3,13 +3,23 @@ stuff = ["abc", "def", "ghi", "jkl", "mno", "pqr"]
 
 def example(i, x={}):
     x[i] = stuff[i]
+    print(x)
+    return x
+
+
+def another(i):
+    x = {}
+    x[i] = stuff[i]
+    print(x)
     return x
 
 
 if __name__ == "__main__":
     for i in range(6):
         mydict = example(i)
-        print(mydict)
+    print()
+    for i in range(6):
+        mydict2 = another(i)
 
 """
 Output:
@@ -19,4 +29,11 @@ Output:
 {0: 'abc', 1: 'def', 2: 'ghi', 3: 'jkl'}
 {0: 'abc', 1: 'def', 2: 'ghi', 3: 'jkl', 4: 'mno'}
 {0: 'abc', 1: 'def', 2: 'ghi', 3: 'jkl', 4: 'mno', 5: 'pqr'}
+
+{0: 'abc'}
+{1: 'def'}
+{2: 'ghi'}
+{3: 'jkl'}
+{4: 'mno'}
+{5: 'pqr'}
 """
